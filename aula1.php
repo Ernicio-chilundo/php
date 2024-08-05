3c131cfb8531db077319995b399b2fdb17929f18
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-05 23:23:53 +0200

    inicia a aula dois do php

--- aula1.php
+++ aula1.php
@@ -1,39 +1,42 @@
 <?php
 
- $dados = [
-    ["Clara","F","Arcadia"],
-    ["Ernicio","M","Mocambique"],
-    ["Gretta","F","Angola"],
-    ["Jim","M","Arcadia"]
- ]
+$dados = [
+    ["Clara", "F", "Arcadia"],
+    ["Ernicio", "M", "Mocambique"],
+    ["Gretta", "F", "Angola"],
+    ["Jim", "M", "America"],
+];
 
- ?>
+?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TABELA</title>
- </head>
- <body>
-    <table border="5px">
-    <thead>
-        <tr>
-        <th>Nome</th>
-        <th>Genero</th>
-        <th>pais</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($dados as $item): ?>
+</head>
+
+<body>
+    <table border="5">
+        <thead>
             <tr>
-                <td><?=$item[0] ?></td>
-                <td><?=$item[1] ?></td>
-                <td><?=$item[3] ?></td>
+                <th>Nome</th>
+                <th>Genero</th>
+                <th>pais</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
+        </thead>
+        <tbody>
+            <?php foreach ($dados as $item) : ?>
+                <tr>
+                    <td><?= $item[0] ?></td>
+                    <td><?= $item[1] ?></td>
+                    <td><?= $item[2] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
- </body>
- </html>
+</body>
+
+</html>
