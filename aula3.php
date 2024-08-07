56b4bac3c1188beef511096547a8e7b3a53ccff2
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-07 07:15:05 +0200

    artmetica matematica

---
+++ aula3.php
@@ -0,0 +1,27 @@
+<?php
+ // Artmitica (Matematica)
+ $num1 = 25;
+ $num2 = 5;
+
+ $res = $num1 + $num2;
+
+ echo "a soma de $num1 e $num2 e igual a $res ";
+
+ $res = $num1 - $num2;
+ echo "A subtracao de res e  igual a $res"
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 3 de php - operacoes com variaveis</title>
+</head>
+<body>
+    
+</body>
+</html>
