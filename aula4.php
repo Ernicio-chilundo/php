355b17fa4dfcd688da84f3a708dc22666ffb70be
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-08 00:13:33 +0200

    usado variavel de um outro conjunto de array

--- aula4.php
+++ aula4.php
@@ -1,30 +1,34 @@
 <?php
 //Arrays >> e um conjunto de variaveis do mesmo tipo
 
-$vet = array(6);
-$vet2 = array("nome"=>"Komso", "cidade"=>"Maxixe","curso"=>"PHP");
+$vet = array(7);
+$vet2 = array("nome" => "Komso", "cidade" => "Maxixe", "curso" => "PHP");
 
-$vet[0]= "Carro";
+$vet[0] = "Carro";
 $vet[1] = "Aviao";
 $vet[2] = "Navio";
 $vet[3] = "Moto";
 $vet[4] = "Onibus";
-$vet[4] = 123456;
+$vet[5] = 123456;
+$vet[6] = $vet2["nome"];
 
-$i =1;
-echo" O elemento do indice $i = " . $vet[ 1]."<br/><hr/>";
+$i = 6;
+echo " O elemento do indice $i = " . $vet[$i] . "<br/><hr/>";
 
-echo "Nome: ".$vet2["nome"]. " - Cidade: ".$vet2["cidade"] . " - Curso: ". $vet2["curso"];
+echo "Nome: " . $vet2["nome"] . " - Cidade: " . $vet2["cidade"] . " - Curso: " . $vet2["curso"];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula 4 php - Vetores/Arrays</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
