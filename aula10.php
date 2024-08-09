960011b7e5c0adbeba10f9c5d9227c9b2768ddd0
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-10 00:41:29 +0200

    aprendendo a usar o loop foreach o foreach comparado com os outros loop e muito simplifica e facil de copreender nao precisa de nenhuma variavel de inicializao/ control...

---
+++ aula10.php
@@ -0,0 +1,20 @@
+<?php
+$transport= array("Carro","Moto","Bicicleta","navio","Onibus","Aviao");
+foreach($transport as $one){
+    echo "$one <br/>";
+    if($one == "Bicicleta")
+    break;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 10 php - loop foreach</title>
+</head>
+<body>
+    
+</body>
+</html>
