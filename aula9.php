43e3ed041c762f7c6d15b43b2da77f5deb0db524
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-09 23:24:18 +0200

    apreendendo a usar o loop principal em todas liguas "for"

--- aula9.php
+++ aula9.php
@@ -1,5 +1,10 @@
 <?php
+$i = 0;
+$tam =5;
 
+for($i=0; $i < $tam ; $i++){
+    echo "Valor de i : $i <br/>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula php - </title>
+    <title>Aula php - loop for</title>
 </head>
 <body>
     
