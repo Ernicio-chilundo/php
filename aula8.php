2d812149b943ebfc70ab09b568a37f5028fd1dac
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-09 22:42:11 +0200

    apreendedo a usar o do while que traduzido para portugues significa "faca"

---
+++ aula8.php
@@ -0,0 +1,28 @@
+<?php 
+$i = 8;
+$tam = 5;
+
+echo "Loop do while<br/>";
+
+do{
+    echo "<pre>O valor da variavel i e : $i <br/>";
+    echo " O valor da varial tam e : $tam <br/></pre>";
+    $i++;
+}while($i < $tam);
+
+echo "<hr/>";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 8 php - loop do while</title>
+</head>
+<body>
+    
+</body>
+</html>
