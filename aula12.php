4be051139831f6157bef7ce013bd0cd20ed32d63
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-10 03:38:13 +0200

    a aula 12 envolveu funcoes sem paramentro, funcoes com paramentro e funcoes com return usado vetor para realizar os calculos fora da funcao

--- aula12.php
+++ aula12.php
@@ -5,9 +5,34 @@ function aula(){
 }
 
 aula();
-aula();
-aula();
-aula();
+
+function mult ($n1,$n2){
+    $res=$n1 * $n2;
+    echo "O produto de $n1 e $n2 = $res<br/>";
+}
+
+mult(5,25);
+
+function mult2 ($n1,$n2){
+    $res = $n1 * $n2;
+    return $res;
+}
+
+$mt = mult2(10,10);
+echo "O resultado da multiplicao e : $mt <br/>";
+
+$valores=array(1,2,3,4,7,46,32,8,11,22);
+function media($val){
+    $tam = count($val);
+    $soma = 0;
+    for ($i = 0; $i < $tam ; $i ++){
+        $soma += $val[$i];
+    }
+    return $soma/$tam;
+}
+
+$med = media($valores);
+echo "Media = $med";
 ?>
 
 <!DOCTYPE html>
