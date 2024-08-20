a21311ab8a7c1f7b01e3cc62f52c54ed6dc303b3
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-20 17:50:44 +0200

    aprendendo a usar as funcoes recursivas

--- aula14.php
+++ aula14.php
@@ -1,5 +1,25 @@
 <?php
+ function aula($num){
 
+    if($num!=0){
+        echo "funcao aula com valor $num <br/>";
+        aula($num-1);
+    }
+}
+// aula(10); 
+
+
+function fat($num){
+    if($num < 0){
+        return -1;
+    }
+    if($num <= 1){
+        return 1;
+    }
+    return $num*fat($num-1);
+}
+
+echo "factorial de -50 igual a: ".fat(-50)
 ?>
 
 <!DOCTYPE html/>
@@ -7,7 +27,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-compatible" edge="IE">
-        <meta name="viewport" content="width=device, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Aula 14 php - funcoes recursivas</title>
     </head>
 
