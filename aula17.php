180182aaa421a3e60076b471c12a5883e6b9c597
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-08-20 21:34:53 +0200

    usado a funcao isset para verficar se a variavel aula foi definida ou nao

--- aula17.php
+++ aula17.php
@@ -1,3 +1,25 @@
 <?php
+    $aula ="";
 
+    if(isset($aula)){
+        echo "Variavel aula definida";
+    }else{
+        echo "Variavel Nao definida";
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 17 php - funcao isset</title>
+</head>
+<body>
+    <form name="f_aula" method="post" action="aula17.php">
+        <label>Nome:</label>
+        <input type="text" name="f_nome"><br/>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
